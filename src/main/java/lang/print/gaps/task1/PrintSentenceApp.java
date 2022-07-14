424b234345114acdab25983b054f9bfd4e233973
MuhammadjonR <100756194+MuhammadjonR@users.noreply.github.com>
2022-07-14 09:40:26 +0500

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task1;
 public class PrintSentenceApp {
     public static void main(String[] args) {
      System.out.println("fixme");
-      System.out.println("I'm updated" + "\n" + "can be compiled. And can write to console");
+      System.out.println("java" + \n + "is" + \n +"smth");
     }
 }
